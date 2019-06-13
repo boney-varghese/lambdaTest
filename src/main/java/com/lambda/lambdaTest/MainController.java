28eaa6fb85c462c98e0e26ac9ff04ae0e371e9e9
boneyqb <boney@qburst.com>
2019-06-13 15:55:04 +0530

    change

--- src/main/java/com/lambda/lambdaTest/MainController.java
+++ src/main/java/com/lambda/lambdaTest/MainController.java
@@ -2,14 +2,13 @@ package com.lambda.lambdaTest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping("/test")
-	public String test(@RequestParam String name) {
+	@RequestMapping("/")
+	public String test() {
 		
-		return "Hello "+name;
+		return "Hello World";
 	}
 }
